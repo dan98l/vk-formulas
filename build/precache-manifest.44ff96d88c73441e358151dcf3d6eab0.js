6b5569e0178429ad011adafef5d6b0b73fa9fda3
Daniil <dan98l@mail.ru>
2020-07-18 00:00:17 +0700

    first

---
+++ build/precache-manifest.44ff96d88c73441e358151dcf3d6eab0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f406edcd712761d37d27d6dad5de15",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7eb1e826edd07f84cfa",
+    "url": "./static/css/2.ed87aa75.chunk.css"
+  },
+  {
+    "revision": "a5d63d30a071c191cce8",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f7eb1e826edd07f84cfa",
+    "url": "./static/js/2.a2b74ccb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a2b74ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d63d30a071c191cce8",
+    "url": "./static/js/main.2e97acdf.chunk.js"
+  },
+  {
+    "revision": "9f774e4993995c628ee1",
+    "url": "./static/js/runtime-main.7dbd0981.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
